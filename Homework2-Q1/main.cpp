801b1cef01e1ceb8869bbfb21438287a0ed23518
RajShah-94 <rajshah94@gmail.com>
2014-01-30 21:35:03 +0000

    Final

---
+++ Homework2-Q1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 30 Jan 2014
+ *      Author: Raj Shah
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a = 1;
+	int b = 6;
+	int c = ((b-a)/2)+a;
+	cout << c << endl;
+}
