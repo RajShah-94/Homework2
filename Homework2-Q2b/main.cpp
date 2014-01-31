08f0ebe9e6a0db4537b455c7555a200e1fda22e9
RajShah-94 <rajshah94@gmail.com>
2014-01-31 00:32:16 +0000

    Final

---
+++ Homework2-Q2b/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 30 Jan 2014
+ *      Author: rajru_000
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	float x = 2;
+	float a;
+	float b = 4;
+	float c = 5;
+	float d = 6.5;
+	float y = (a*pow(x,3)) + (b*pow(x,2)) + (c*x) +d;
+
+	cout << y << endl;
+}
