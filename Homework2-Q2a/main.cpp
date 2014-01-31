2fd19a9d397a9a10f18265e08d64a0d75abf7317
RajShah-94 <rajshah94@gmail.com>
2014-01-31 00:35:07 +0000

    Final

--- Homework2-Q2a/main.cpp
+++ Homework2-Q2a/main.cpp
@@ -11,26 +11,12 @@
 using namespace std;
 
 int main() {
-	float x;
-	float a;
-	float b;
-	float c;
-	float d;
-	double i;
-
-	cin >> a;
-	cin >> b;
-	cin >> c;
-	cin >> d;
-
+	float x = 2;
+	float a = 3;
+	float b = 4;
+	float c = 5;
+	float d = 6.5;
 	float y = (a*pow(x,3)) + (b*pow(x,2)) + (c*x) +d;
 
-	for(x=-10; x<=10; i++) {
-		cout.setf(ios::scientific,
-				ios::floatfield);
-		cout.precision(3);
-		cout << y << endl;
-		float x=x+0.5i;
-	}
-
+	cout << y << endl;
 }
